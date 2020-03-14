a26595bd7d63719c98c186aa557c77f8dd593b15
Adilson Lima de Melo Junior <adilsonlimejr17@gmail.com>
2020-03-14 14:14:08 -0300

    instalacao apis

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,17 @@
 const functions = require('firebase-functions');
+var admin = require("firebase-admin");
 
+var serviceAccount = require("./ceresthon.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://fluttertestes-1381d.firebaseio.com"
+})
  //https://firebase.google.com/docs/functions/write-firebase-functions
  exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
  });
+
+ exports.funcao = functions.https.onRequest((req,res)=>{
+
+ })
